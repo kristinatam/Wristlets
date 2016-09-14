44e414704096af1278c86ad7567d272125915485
kristinatam <kristina.tamulyte@gmail.com>
2016-09-14 19:22:04 +0300

    Registration form, RWD
    Registration form added to button "Registration", Responsive web design
    added

---
+++ registration.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('button').click (function() {
+
+    var email = $('#email').val();
+    var username = $('#username').val();
+    var password = $('#password').val();
+
+    if (email == '' || username == '' || password == '') {
+    alert("Please fill in all fields!");
+    }  else {
+    $('.success').show(); }
+
+    });
+});
