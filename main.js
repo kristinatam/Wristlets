f666694259d5371f5c36771650c940696166a0de
kristinatam <kristina.tamulyte@gmail.com>
2016-10-13 18:42:09 +0300

    HTML,JS changes
    HTML,JS changes-alignment,section names, JS code change

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-    var colors = $.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?",
-    { numResults: 3 }, function() {
 
-        var allColors = colors.responseJSON; 
-        
-        $('#rotated').click(function() {  
-        var randomColor = allColors[Math.floor(Math.random() * allColors.length)];
-            $('body, address').css("background-color","#"+ randomColor.colors[0]);    
-            console.log("Spalva: ", randomColor.colors[0]);
-            console.log("Spalvos: ", allColors);
-});
+    $('#rotated').click(function() {
+        var colors = $.getJSON("http://www.colourlovers.com/api/palettes/top?jsonCallback=?", {
+            numResults: 3
+        }, function() {
+
+            var allColors = colors.responseJSON;
+
+            var randomColor = allColors[Math.floor(Math.random() * allColors.length)];
+            $('body, address').css("background-color", "#" + randomColor.colors[0]);
+        });
     });
-      
-         });
+
+});
--- registration.js
+++ registration.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-    $('#button').click (function() {
+    $('#button').click(function() {
 
-    var email = $('#email').val();
-    var username = $('#username').val();
-    var password = $('#password').val();
+        var email = $('#email').val();
+        var username = $('#username').val();
+        var password = $('#password').val();
 
-    if (email == '' || username == '' || password == '') {
-    alert("Please fill in all fields!");
-    }  else {
-    $('.success').show(); }
-
+        if (email == '' || username == '' || password == '') {
+            alert("Please fill in all fields!");
+        } else {
+            $('.success').show();
+            event.preventDefault();
+        }
     });
 });
